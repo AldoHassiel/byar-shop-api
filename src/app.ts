502ad934aba89e89be174866ea7064a3e346b775
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-14 21:27:51 -0700

    Añade cookie parser para poder leer el token

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import rutaTest from "@/modulos/test/test.ruta.js";
 import rutaAutenticacion from "@/modulos/autenticacion/autenticacion.ruta.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(rutaTest);
 app.use(rutaAutenticacion);
