93b0026732368409ccad83b8faf7d07e146e3b69
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-16 16:44:44 -0700

    Update global.ts

--- src/config/global.ts
+++ src/config/global.ts
@@ -1,4 +1,14 @@
+import type { Token } from "@/utilidades/token.js";
+
 export const PUERTO = process.env.PUERTO || 8500;
 export const DB_PUERTO = Number(process.env.DB_PUERTO) || 5432;
 export const SECRETO_JWT = process.env.SECRETO_JWT || "CreaElArchivoPuntoEnv";
 export const SAL = Number(process.env.SAL_JWT) || 10;
+
+declare global {
+  namespace Express {
+    interface Request {
+      usuario?: Token;
+    }
+  }
+}
