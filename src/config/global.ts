8a267f43a3bb5589e236a91c764d0a9af601b718
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-16 13:34:05 -0700

    Update global.ts

--- src/config/global.ts
+++ src/config/global.ts
@@ -1,4 +1,4 @@
 export const PUERTO = process.env.PUERTO || 8500;
 export const DB_PUERTO = Number(process.env.DB_PUERTO) || 5432;
 export const SECRETO_JWT = process.env.SECRETO_JWT || "CreaElArchivoPuntoEnv";
-export const SAL_JWT = Number(process.env.SAL_JWT) || 10;
+export const SAL = Number(process.env.SAL_JWT) || 10;
