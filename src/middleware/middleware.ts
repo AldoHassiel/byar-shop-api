6f4383126eb605e393e542d2c7266fcf744e8fac
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-18 20:26:58 -0700

    Update middleware.ts

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -16,6 +16,40 @@ const leerToken = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
+const eresEseUsuario = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return respuestaError(res, "Token de usuario requerido", 498);
+  }
+
+  if (!req.params.usuarioId) {
+    return respuestaError(res, "Token de usuario requerido", 498);
+  }
+
+  try {
+    const datos = jwt.verify(token, SECRETO_JWT) as Token;
+
+    if (datos.es_admin) {
+      req.usuario = {
+        id: Number(req.params.usuarioId),
+        es_admin: true,
+        correo: datos.correo,
+      };
+      return next();
+    }
+
+    if (datos.id !== Number(req.params.usuarioId)) {
+      return respuestaError(res, "No eres ESE usuario", 401);
+    }
+
+    req.usuario = datos;
+    return next();
+  } catch (error) {
+    return respuestaError(res, "Token inválido o expirado", 498);
+  }
+};
+
 const eresAdmin = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.token;
 
@@ -36,33 +70,8 @@ const eresAdmin = (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-const eresEl = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies.token;
-  const id = req.body.id;
-
-  if (!token) {
-    return respuestaError(res, "Token de usuario requerido", 498);
-  }
-
-  try {
-    const datos = jwt.verify(token, SECRETO_JWT) as Token;
-
-    if (datos.es_admin) {
-      return next();
-    }
-
-    if (datos.id !== id) {
-      return respuestaError(res, "No eres ESE cliente, ¿Quien eres?", 401);
-    }
-
-    next();
-  } catch (error) {
-    return respuestaError(res, "Token invalido o expirado", 498);
-  }
-};
-
 export const Middle = {
-  eresEl,
   eresAdmin,
+  eresEseUsuario,
   leerToken,
 };
