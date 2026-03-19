f8d4200a59644378fcae8be0af92bd1fcf64761a
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-19 11:01:02 -0700

    Yaak y Swagger

--- src/servidor.ts
+++ src/servidor.ts
@@ -1,5 +1,9 @@
 import app from "@/app.js";
 import { PUERTO } from "@/config/global.js";
+import swaggerUI from "swagger-ui-express";
+import swaggerOutput from "swagger-output.json" with { type: "json" };
+
+app.use("/documentacion", swaggerUI.serve, swaggerUI.setup(swaggerOutput));
 
 app.listen(PUERTO, () => {
   console.log(`El servidor se esta ejecutando en el puerto ${PUERTO}`);
