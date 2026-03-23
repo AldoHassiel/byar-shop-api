b126a036562bb25b45778a54ccd06ff19ff5b0c0
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-22 17:18:25 -0700

    ¿Ya cors?

--- src/app.ts
+++ src/app.ts
@@ -46,9 +46,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173/", FRONT_URL],
-    methods: "*",
-    allowedHeaders: "*",
+    origin: ["http://localhost:5173", FRONT_URL],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
