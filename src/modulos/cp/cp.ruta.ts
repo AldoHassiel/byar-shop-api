6aeffd6ffca5d7959843400992085bee17bf49bb
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-05-11 00:00:36 -0700

    Los codigos postales ahora ya se encuentran en la base de datos

--- src/modulos/cp/cp.ruta.ts
+++ src/modulos/cp/cp.ruta.ts
@@ -1,3 +1,4 @@
+import { db } from "@/config/db.js";
 import { respuestaError, respuestaOk } from "@/utilidades/respuesta.js";
 import { Router } from "express";
 
@@ -5,46 +6,28 @@ const rutasCP = Router();
 
 rutasCP.get("/cp/:cp", async (req, res) => {
   try {
-    const consulta = await fetch(
-      `https://sepomex.icalialabs.com/api/v1/zip_codes?zip_code=${req.params.cp}`,
-    );
+    const codigo_postal = req.params.cp;
 
-    interface ZipCodes {
-      d_estado: string;
-      d_mnpio: string;
-      d_ciudad: string;
-      d_asenta: string;
-    }
+    const query = `
+        SELECT
+          ARRAY_AGG(DISTINCT d_estado ORDER BY d_estado) AS estados,
+          ARRAY_AGG(DISTINCT d_mnpio ORDER BY d_mnpio) AS municipios,
+          ARRAY_AGG(DISTINCT d_ciudad ORDER BY d_ciudad) FILTER (WHERE d_ciudad IS NOT NULL AND d_ciudad <> '') AS ciudades,
+          ARRAY_AGG(DISTINCT d_asenta ORDER BY d_asenta) AS colonias
+        FROM codigos_postales
+        WHERE d_codigo = $1;
+      `;
 
-    const { zip_codes }: { zip_codes: ZipCodes[] } = await consulta.json();
+    const { rows } = await db.query(query, [codigo_postal]);
 
-    const obtenerValoresUnicos = (
-      zip_codes: ZipCodes[],
-      llave: keyof ZipCodes,
-    ) => {
-      const todosLosValores = zip_codes.map((dato) => dato[llave]);
-      const sinDuplicados = new Set(todosLosValores);
-      const comoArray = Array.from(sinDuplicados);
-      return comoArray;
-    };
-
-    const estados = obtenerValoresUnicos(zip_codes, "d_estado");
-    const municipios = obtenerValoresUnicos(zip_codes, "d_mnpio");
-    const ciudades = obtenerValoresUnicos(zip_codes, "d_ciudad");
-    const colonias = obtenerValoresUnicos(zip_codes, "d_asenta");
-
-    const info = {
-      estados,
-      municipios,
-      ciudades,
-      colonias,
-    };
-
-    return respuestaOk(
-      res,
-      "Datos del codigo postal obtenidos con exito",
-      [info],
-    );
+    respuestaOk(res, "Datos del codigo postal obtenidos con exito", [
+      {
+        estados: rows[0].estados || [],
+        municipios: rows[0].municipios || [],
+        ciudades: rows[0].ciudades || [],
+        colonias: rows[0].colonias || [],
+      },
+    ]);
   } catch (error) {
     console.log(error);
     return respuestaError(res, "Error interno del servidor");
